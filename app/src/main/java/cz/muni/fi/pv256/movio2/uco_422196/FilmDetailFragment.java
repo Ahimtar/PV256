17ae44344b576503b3ea5247afdd1c805a1ee6f7
Ahimtar <misoromanek@gmail.com>
2017-12-04 04:17:11 +0100

    Work in progress on task 3

---
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco_422196/FilmDetailFragment.java
@@ -0,0 +1,57 @@
+package cz.muni.fi.pv256.movio2.uco_422196;
+
+import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Ja on 4.12.2017.
+ */
+
+public class FilmDetailFragment extends Fragment {
+
+    public static final String TAG = FilmDetailFragment.class.getSimpleName();
+    private static final String ARGS_FILM = "args_film";
+
+    private Context mContext;
+    private Film mFilm;
+
+    public static FilmDetailFragment newInstance(Film film) {
+        FilmDetailFragment fragment = new FilmDetailFragment();
+        Bundle args = new Bundle();
+        args.putParcelable(ARGS_FILM, film);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getActivity();
+        Bundle args = getArguments();
+        if (args != null) {
+            mFilm = args.getParcelable(ARGS_FILM);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        TextView titleTv = (TextView) view.findViewById(R.id.detail_film);
+        TextView titleLowTv = (TextView) view.findViewById(R.id.detail_film_low);
+
+        if (mFilm != null) {
+            titleTv.setText(mFilm.getTitle());
+            titleLowTv.setText(mFilm.getCoverPath());
+        }
+
+        return view;
+    }
+}
--- app/src/main/java/cz/muni/fi/pv256/movio2/uco_422196/PV256aktivita.java
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco_422196/PV256aktivita.java
@@ -1,13 +1,53 @@
 package cz.muni.fi.pv256.movio2.uco_422196;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
 
-public class PV256aktivita extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class PV256aktivita extends AppCompatActivity
+        implements FilmListFragment.OnFilmSelectListener {
+
+        private boolean mTwoPane;
+
+        /** Called when the activity is first created. */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            //setContentView(R.layout.news_articles);
+
+            setContentView(R.layout.activity_pv256aktivita);
+            if (findViewById(R.id.film_detail_container) != null) {
+                mTwoPane = true;
+                if (savedInstanceState == null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.film_detail_container, new FilmDetailFragment(), FilmDetailFragment.TAG)
+                            .commit();
+                }
+            } else {
+                mTwoPane = false;
+                getSupportActionBar().setElevation(0f);
+            }
+        }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pv256aktivita);
+    public void onFilmSelect(Film film) {
+        if (mTwoPane) {
+            FragmentManager fm = getSupportFragmentManager();
+            FilmDetailFragment fragment = FilmDetailFragment.newInstance(film);
+            fm.beginTransaction()
+                    .replace(R.id.film_detail_container, fragment, FilmDetailFragment.TAG)
+                    .commit();
+        } else {
+            Intent intent = new Intent(this, FilmDetailActivity.class);
+            intent.putExtra(FilmDetailActivity.EXTRAFILM, film);
+            startActivity(intent);
+        }
     }
+
 }
