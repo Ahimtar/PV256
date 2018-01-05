ae8924ecef4a4ec6cbce0611c1f2ab26b5874cb5
Ahimtar <misoromanek@gmail.com>
2018-01-06 00:26:18 +0100

    Fixed a major multiple-movie-appearing bug, task 4 done

--- app/src/main/java/cz/muni/fi/pv256/movio2/uco_422196/RecyclerViewAdapter.java
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco_422196/RecyclerViewAdapter.java
@@ -21,7 +21,7 @@ import java.util.List;
  * Created by Ja on 29.12.2017.
  */
 
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
 
     private Context mAppContext;
     private List<Object> mFilmList;
@@ -44,24 +44,37 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater inflater = (LayoutInflater) mAppContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.list_item_film, parent, false);
-        TextView text = (TextView) view.findViewById(R.id.list_item_text);
-        Log.d("Inflating", "Inflating " + parent.getId());
-        return new ViewHolder(view);
+    public int getItemViewType(int position) {
+        return mFilmList.get(position) instanceof Film ? 1 : 0;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewIsFilm) {
+        LayoutInflater inflater = (LayoutInflater) mAppContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        Log.d("Inflating", "Inflating " + parent.getId());
+        View view;
+        if (viewIsFilm == 0){
+            view = inflater.inflate(R.layout.category, parent, false);
+            //TextView text = (TextView) view.findViewById(R.id.list_item_text);
+            return new CategoryViewHolder(view);
+        } else {
+            view = inflater.inflate(R.layout.list_item_film, parent, false);
+            return new FilmViewHolder(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
         if(mFilmList.get(position) instanceof Film) {
+
+            FilmViewHolder filmHolder = (FilmViewHolder) holder;
             Film film = (Film) mFilmList.get(position);
             Log.d("Binding", "Binding " + film.getTitle());
-            holder.text.setText(film.getTitle());
-            holder.popularity.setText(String.valueOf(film.getPopularity()));
+            filmHolder.text.setText(film.getTitle());
+            filmHolder.popularity.setText(String.valueOf(film.getPopularity()));
             int coverId = mAppContext.getResources().getIdentifier(film.getSmallPath(), "drawable", mAppContext.getPackageName());
             Drawable cover = mAppContext.getResources().getDrawable(coverId);
-            holder.coverImageView.setImageDrawable(cover);
+            filmHolder.coverImageView.setImageDrawable(cover);
 
             holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -74,28 +87,37 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             int backgroundColorOpaque = palette.getDarkVibrantColor(0x000000);
             int backgroundColorTransparent = Color.argb(128, Color.red(backgroundColorOpaque), Color.green(backgroundColorOpaque), Color.blue(backgroundColorOpaque));
             GradientDrawable gd = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{Color.TRANSPARENT, backgroundColorOpaque});
-            holder.text.setBackgroundColor(backgroundColorTransparent);
-            holder.popularity.setBackgroundColor(backgroundColorOpaque);
-            holder.itemView.setTag(film);
+            filmHolder.text.setBackgroundColor(backgroundColorTransparent);
+            filmHolder.popularity.setBackgroundColor(backgroundColorOpaque);
+            filmHolder.itemView.setTag(film);
         }
         else
         {
-            holder.text.setTextColor(0xff0000ff);
-            holder.text.setText((String) mFilmList.get(position));
+            CategoryViewHolder categoryHolder = (CategoryViewHolder) holder;
+            categoryHolder.text.setText((String) mFilmList.get(position));
         }
 
     }
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    static class FilmViewHolder extends RecyclerView.ViewHolder {
         private ImageView coverImageView;
         public TextView text;
         public TextView popularity;
 
-        public ViewHolder(View view) {
+        public FilmViewHolder(View view) {
             super(view);
             text = (TextView) itemView.findViewById(R.id.list_item_text);
             coverImageView = (ImageView) view.findViewById(R.id.list_item_icon);
             popularity = (TextView) itemView.findViewById(R.id.list_item_popularity);
+        }
+    }
+
+    static class CategoryViewHolder extends RecyclerView.ViewHolder {
+        public TextView text;
+
+        public CategoryViewHolder(View view) {
+            super(view);
+            text = (TextView) itemView.findViewById(R.id.category_type);
         }
     }
 }
