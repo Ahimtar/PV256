7634cb07b499069aea672913ecd9d2cbf48a7bcc
Ahimtar <misoromanek@gmail.com>
2018-01-07 02:45:56 +0100

    Film descriptions now correctly show on film detail pages

--- app/src/main/java/cz/muni/fi/pv256/movio2/uco_422196/FilmDetailFragment.java
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco_422196/FilmDetailFragment.java
@@ -54,6 +54,7 @@ public class FilmDetailFragment extends Fragment {
         if (mFilm != null) {
             titleTv.setText(mFilm.getTitle());
             titleLowTv.setText(mFilm.getCoverPath());
+            descTv.setText(mFilm.getDescription());
             Picasso.with(mContext).load("https://image.tmdb.org/t/p/w500/" + mFilm.getCoverPath()).into(imageView);
         }
 
