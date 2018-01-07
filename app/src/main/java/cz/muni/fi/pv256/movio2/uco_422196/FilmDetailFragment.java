258bfe9ee325ba52a2608c0d2b761b7eb0c4433b
Ahimtar <misoromanek@gmail.com>
2018-01-07 14:34:22 +0100

    Added side image into fragment detail (correction from task 6)

--- app/src/main/java/cz/muni/fi/pv256/movio2/uco_422196/FilmDetailFragment.java
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco_422196/FilmDetailFragment.java
@@ -50,12 +50,14 @@ public class FilmDetailFragment extends Fragment {
         TextView titleLowTv = (TextView) view.findViewById(R.id.detail_film_low);
         TextView descTv = (TextView) view.findViewById(R.id.detail_film_desc);
         ImageView imageView = (ImageView) view.findViewById(R.id.detail_icon);
+        ImageView imageSmallView = (ImageView) view.findViewById(R.id.detail_small_icon);
 
         if (mFilm != null) {
             titleTv.setText(mFilm.getTitle());
             titleLowTv.setText(mFilm.getCoverPath());
             descTv.setText(mFilm.getDescription());
             Picasso.with(mContext).load("https://image.tmdb.org/t/p/w500/" + mFilm.getCoverPath()).into(imageView);
+            Picasso.with(mContext).load("https://image.tmdb.org/t/p/w500/" + mFilm.getSmallPath()).into(imageSmallView);
         }
 
         return view;
